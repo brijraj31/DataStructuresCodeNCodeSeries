c44c8f8b66853a05f182d3b18f6ef7aac409c080
Brij <22271048+brijraj31@users.noreply.github.com>
2020-05-12 15:54:34 +0530

    Added Square Root Decomposition Course

---
+++ SquareRootDecompostion/L01Lec1/RangeMinimumQuery.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define MAXN 100000
+int arr[MAXN];
+int sTable[317];
+
+void initialize(int n) {
+    int sqTableSize = ceil(sqrt(n));
+
+    
+}
+int main() {
+
+    int size;
+    cin >> size;
+
+    for(int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+
+
+}
